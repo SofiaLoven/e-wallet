0c9b3ea38110e1509dd39f6a14609d77aa26a0ed
SofiaLoven <sofia.loven93@gmail.com>
2023-09-22 14:16:18 +0200

    functionalities done

--- my-project/src/features/cards/NewCard.jsx
+++ my-project/src/features/cards/NewCard.jsx
@@ -13,7 +13,7 @@ export const NewCard =()=>{
     const [expireMonth, setExpireMonth] = useState(null);
     const [expireYear, setExpireYear] = useState(null);
     const [cvv, setCvv] = useState(null);
-    //const errorTxt = document.querySelector("#error");
+    const errorExp = document.querySelector("#expDate");
     
     let card = {
         vendor, 
@@ -61,18 +61,47 @@ export const NewCard =()=>{
                     <div>
                         <label >Valid thru
                         <input onChange={(e)=>{
-                            setExpireMonth(e.target.value);
+                            let expMonth = e.target.value;
+                            if(expMonth > 12 || expMonth <= 0){
+                                errorExp.innerText= "On what planet are you? This planet has only 12 months"
+                            }else{
+                                errorExp.innerText="";
+                                if(expMonth<10){
+                                    let newExpMonth = `0${expMonth}`;
+                                    setExpireMonth(newExpMonth);
+                                }else{
+                                    errorExp.innerText="";
+                                    setExpireMonth(expMonth);
+                                }
+                            }
                           
-                        }} type="number" />
+                        }} type="number" max={12} min={0}/>
                         <input onChange={(e)=>{
-                            setExpireYear(e.target.value)
-                            }} type="number"/>
-                        </label>
+                            let expYear = e.target.value;
+                            if(expYear<23){
+                                errorExp.innerText= "Please choose a yeare in the future.."
+                            }else if(expYear>100){
+                                errorExp.innerText = "This is not at year."
+                            }else{
+                                errorExp.innerText="";
+                                setExpireYear(expYear);
+                            }
+                            }} type="number" min={23}/>
+                        </label><p id="expDate"></p>
                         <label >CVV
                         <input onChange={(e)=>{
-                            setCvv(e.target.value);
-                            }} type="number"/>
-                        </label>
+                            let cvv = e.target.value;
+                            let errorCvv = document.querySelector("#cvv");
+                            if(cvv > 999){
+                                errorCvv.innerText= "You can only have three digits";
+                            }else if(cvv < 100 ){
+                                errorCvv.innerText="You need three digits";
+                            }else{
+                                errorCvv.innerText=""
+                                setCvv(cvv);
+                            }
+                            }} type="number" max={999} min={0}/>
+                        </label><p id="cvv"></p>
                     </div>
                     <label>Vendor
                         <select onChange={(e)=>{
@@ -92,4 +121,3 @@ export const NewCard =()=>{
     )
 }
 
-//Skicka Form med klick?? preventDefault
